e7f769630110bc913a2edad47ad54c51f92f829e
hsneden <48335502+hcsneden@users.noreply.github.com>
2024-05-04 15:01:31 -0500

    change icon and data table

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,28 +1,34 @@
 import moment from "moment"
-import { Container, Form, Row, Col } from "react-bootstrap"
+import { Container, Form, Row, Col, Table } from "react-bootstrap"
 
 export const History = (props) => {
     const pastRuns = props.data
-    const sorted = pastRuns.sort(function(a,b){
+    const sorted = pastRuns.sort(function (a, b) {
         return new Date(b.date) - new Date(a.date);
-      });
+    });
 
 
     return (
-        <Container style={{paddingBottom: '5%'}}>
-            <Row>
-                <Col md={{span: 3, offset: 3}} xs={{span: 4, offset: 2}} >Date</Col>
-                <Col md={3} xs={5}>Distance</Col>
-            </Row>
-            {sorted.map((item) => {
-                return (
-                    <Row className="history-data">
-                        <Col md={{span: 3, offset: 3}} xs={{span: 4, offset: 2}} title="Date">{moment(item.date).format("MMM Do")}</Col>
-                        <Col md={3} xs={5} title="Distance">{item.distance}</Col>
-                    </Row>
-                )
 
-            })}
+        <Container style={{ paddingBottom: '5%', padding: '10%' }}>
+            <Table size="sm">
+                <thead>
+                    <tr className="same-col-widths">
+                        <th>Date</th>
+                        <th>Distance</th>
+                    </tr>
+                </thead>
+                <tbody className="history-data">
+                    {sorted.map((item) => {
+                        return (
+                            <tr>
+                                <td>{moment(item.date).format("MMM D")}</td>
+                                <td>{item.distance}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
         </Container>
     )
 }
